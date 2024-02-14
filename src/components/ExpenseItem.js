38625f2f305df8c8328275b12ff200249f8549e9
Muhammad Illyas <anonymous50137@gmail.com>
2024-02-14 13:30:49 -0500

    Finsihed the project

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,31 +1,53 @@
-import React, { useContext } from 'react';
-import { TiDelete } from 'react-icons/ti';
-import { AppContext } from '../context/AppContext';
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 const ExpenseItem = (props) => {
-    const { dispatch, currency } = useContext(AppContext);
-    const handleDeleteExpense = () => {
-        dispatch({
-            type: 'DELETE_EXPENSE',
-            payload: props.id,
-        });
+  const { dispatch, currency } = useContext(AppContext);
+  const increaseAllocation = (name) => {
+    const expense = {
+      name: name,
+      cost: 10,
     };
-    const increaseAllocation = (name) => {
-        const expense = {
-            name: name,
-            cost: 10,
-        };
-        dispatch({
-            type: 'ADD_EXPENSE',
-            payload: expense
-        });
-    }
-    return (
-        <tr>
-        <td>{props.name}</td>
-        <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td className='bg-success rounded-full text-white'><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
-        </tr>
-    );
+    dispatch({
+      type: "ADD_EXPENSE",
+      payload: expense,
+    });
+  };
+
+  const decreaseAllocation = (name) => {
+    const expense = {
+      name: name,
+      cost: 10,
+    };
+    dispatch({
+      type: "RED_EXPENSE",
+      payload: expense,
+    });
+  };
+
+  return (
+    <tr>
+      <td>{props.name}</td>
+      <td>
+        {currency}
+        {props.cost}
+      </td>
+      <td>
+        <button
+          className="addBtn"
+          onClick={(event) => increaseAllocation(props.name)}
+        >
+          +
+        </button>
+      </td>
+      <td>
+        <button
+          className="deleteBtn"
+          onClick={(event) => decreaseAllocation(props.name)}
+        >
+          -
+        </button>
+      </td>
+    </tr>
+  );
 };
 export default ExpenseItem;
